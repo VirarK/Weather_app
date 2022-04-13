23e439ca2d814eeebfa8187fded0c34b2ea6e2ff
GHasnae <gaizi.hasnae@gmail.com>
2022-04-13 18:41:14 +0200

    ajout de la fonction clock + changement de background day-night + changement de couleur du text selon day-night

--- src/js/login_logout.js
+++ src/js/login_logout.js
@@ -41,3 +41,58 @@ function get_login_data() {
 
   // TODO : redirection vers le site principale
 }
+
+setInterval(() => {
+  const time = document.querySelector("#time");
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let day_night = "AM";
+  if (hours > 12) day_night = "PM";
+
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+
+  time.textContent = hours + ":" + minutes + ":" + seconds + " " + day_night;
+});
+
+function changeBg() {
+  let date = new Date();
+  let hours = date.getHours();
+  let whatTime;
+  let bg = document.body;
+  let bg_day = "url(../../icons/bg-log.jpg)";
+  let bg_night = "url(../../icons/night.jpg)";
+
+  if (hours > 9 && hours < 18) {
+    bg.style.backgroundImage = bg_day;
+    bg.style.height = "100%";
+    whatTime = "d";
+  } else {
+    bg.style.background = bg_night;
+    bg.style.height = "100%";
+    whatTime = "n";
+  }
+  return whatTime;
+}
+
+function changeColors() {
+  if (changeBg() == "n") {
+    console.log("night");
+    let cn = document.getElementById("seConnecter");
+    let rm = document.getElementById("rememberMe");
+    let btn = document.getElementById("btn");
+    let gif = document.getElementById("gif");
+
+    cn.style.color = "white";
+    rm.style.color = "white";
+    btn.style.color = "white";
+    gif.style.backgroundImage = "";
+  } else if (changeBg() == "d") {
+    console.log("day");
+  }
+}
+changeColors();
