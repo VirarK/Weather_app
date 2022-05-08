3524405666940719ab86836c83076bbf3354acac
m27bay <mickael.ledenmat@gmail.com>
2022-05-08 23:18:46 +0200

    ajout lien cliquable favori

---
+++ files/public/js/colors.js
@@ -0,0 +1,69 @@
+/* sidePanel functions */
+/*show sidePanel of width 325*/
+function open_nav() {
+	document.getElementById("sidePanel").style.width = "330px";
+}
+
+/* hide sidePanel width 0 */
+function close_nav() {
+	document.getElementById("sidePanel").style.width = "0";
+}
+
+/*change login.hbs bg*/
+function change_bg() {
+	if(document.body) {
+		let date = new Date();
+		let hours = date.getHours();
+		let whatTime;
+		let bg = document.body.style;
+		let bg_day = "url(../images/bg-log.jpg)";
+		let bg_night = "url(../images/night.jpg)";
+	
+		if (hours > 9 && hours < 18) {
+			bg.backgroundImage = bg_day;
+			bg.height = "100%";
+			whatTime = "d";
+		} else {
+			bg.backgroundImage = bg_night;
+			bg.height = "100%";
+			whatTime = "n";
+		}
+		return whatTime;
+	}
+}
+
+/*change login.hbs colors*/
+function change_colors() {
+	if (change_bg() == "n") {
+		//console.log("night");
+		let cn = document.getElementById("seConnecter");
+		let cnx = document.getElementById("cnx");
+		let rs = document.getElementById("rs");
+		let gif = document.getElementById("gif");
+		let time = document.getElementById("time");
+		let sc = document.getElementById("sInscrire");
+
+		if (cn) {
+			cn.style.color = "white";
+		}
+		if (cnx) {
+			cnx.style.color = "white";
+		}
+		if (rs) {
+			rs.style.color = "white";
+		}
+		if (gif) {
+			gif.style.backgroundImage = "";
+		}
+		if (time) {
+			time.style.color = "white";
+		}
+		if (sc) {
+			sc.style.color = "white";
+		}
+	} else if (change_bg() == "d") {
+		//console.log("day");
+	}
+}
+
+change_colors();
--- files/public/js/utils.js
+++ files/public/js/utils.js
@@ -1,88 +1,145 @@
-/* sidePanel functions */
-/*show sidePanel of width 325*/
-function openNav() {
-	document.getElementById("sidePanel").style.width = "330px";
+/**
+ * Return corresponding french day.
+ *
+ * @param {String} day english day.
+ * @returns french day.
+ */
+ function transform_day(day) {
+	const map_day_en_to_fr = {
+		Mon: "Lundi",
+		Tue: "Mardi",
+		Wed: "Mercredi",
+		Thu: "Jeudi",
+		Fri: "Vendredi",
+		Sat: "Samedi",
+		Sun: "Dimanche",
+	};
+  
+	return map_day_en_to_fr[day];
+  }
+  
+/**
+ * Return corresponding french day but shortest version.
+ *
+ * @param {String} day english day.
+ * @returns french day.
+ */
+function transform_day_low(day) {
+	const map_day_en_to_fr = {
+		Mon: "Lun",
+		Tue: "Mar",
+		Wed: "Mer",
+		Thu: "Jeu",
+		Fri: "Ven",
+		Sat: "Sam",
+		Sun: "Dim",
+	};
+
+	return map_day_en_to_fr[day];
 }
 
-/* hide sidePanel width 0 */
-function closeNav() {
-	document.getElementById("sidePanel").style.width = "0";
+/**
+ * Return corresponding french month.
+ *
+ * @param {*} month english month.
+ * @returns french month.
+ */
+function transform_month(month) {
+const map_month_en_to_fr = {
+	Jan: "Janvier",
+	Feb: "Fevrier",
+	Mar: "Mars",
+	Apr: "Avril",
+	May: "Mai",
+	Jun: "Juin",
+	Jul: "Juillet",
+	Aug: "Aout",
+	Sep: "Septembre",
+	Oct: "Octobre",
+	Nov: "Novembre",
+	Dec: "Decembre",
+};
+
+return map_month_en_to_fr[month];
 }
 
-/* Clock */
-setInterval(() => {
-	const time = document.querySelector("#time");
-	if(time != null) {
-		let date = new Date();
-		let hours = date.getHours();
-		let minutes = date.getMinutes();
-		let seconds = date.getSeconds();
-		let day_night = "AM";
-		if (hours > 12) day_night = "PM";
-	
-		if (hours < 10) hours = "0" + hours;
-		if (minutes < 10) minutes = "0" + minutes;
-		if (seconds < 10) seconds = "0" + seconds;
-	
-		time.textContent = hours + ":" + minutes + ":" + seconds + " " + day_night;
+/**
+ * Transform date in french version.
+ *
+ * @param {Date} date english version (ex Mer Mar 16).
+ * @param {Number} mode shortest day or not.
+ * @returns date in french version (ex Mer Mar 16 -> Mercredi 16 Mars or Mer 16 Mars).
+ */
+function transform_date(date, mode = 0) {
+	var date_str = date.toDateString();
+	var table_date_str = date_str.split(" ");
+
+	if (mode == 0) {
+		return (
+		transform_day(table_date_str[0]) +
+		" " +
+		table_date_str[2] +
+		" " +
+		transform_month(table_date_str[1])
+		);
+	} else if (mode == 1) {
+		return transform_day_low(table_date_str[0]) + " " + table_date_str[2];
 	}
-});
+}
 
-/*change login.hbs bg*/
-function change_bg() {
-	if(document.body) {
-		let date = new Date();
-		let hours = date.getHours();
-		let whatTime;
-		let bg = document.body.style;
-		let bg_day = "url(../images/bg-log.jpg)";
-		let bg_night = "url(../images/night.jpg)";
-	
-		if (hours > 9 && hours < 18) {
-			bg.backgroundImage = bg_day;
-			bg.height = "100%";
-			whatTime = "d";
-		} else {
-			bg.backgroundImage = bg_night;
-			bg.height = "100%";
-			whatTime = "n";
-		}
-		return whatTime;
+/**
+ * Return country code corresponding to country name.
+ *
+ * @param {String} country the country code (FR, EN, ...).
+ * @returns the country full name (France, ...).
+ */
+function transform_country(country) {
+	try {
+		let region_names = new Intl.DisplayNames(["en"], { type: "region" });
+		return region_names.of(country);
+	} catch(RangeError) {
+		return country;
 	}
 }
 
-/*change login.hbs colors*/
-function change_colors() {
-	if (change_bg() == "n") {
-		//console.log("night");
-		let cn = document.getElementById("seConnecter");
-		let cnx = document.getElementById("cnx");
-		let rs = document.getElementById("rs");
-		let gif = document.getElementById("gif");
-		let time = document.getElementById("time");
-		let sc = document.getElementById("sInscrire");
+// ##############################################################################################
 
-		if (cn) {
-			cn.style.color = "white";
-		}
-		if (cnx) {
-			cnx.style.color = "white";
-		}
-		if (rs) {
-			rs.style.color = "white";
-		}
-		if (gif) {
-			gif.style.backgroundImage = "";
-		}
-		if (time) {
-			time.style.color = "white";
-		}
-		if (sc) {
-			sc.style.color = "white";
-		}
-	} else if (change_bg() == "d") {
-		//console.log("day");
+/**
+ * Convert celsius in fehrenheit.
+ *
+ * @param {Number} temperature .
+ * @returns celsius.
+ */
+function celsius_to_fahrenheit(temperature) {
+	return (temperature * 9) / 5 + 32;
+}
+
+// ##############################################################################################
+
+/**
+ * Get user location with navigator.
+ */
+function get_location() {
+	if ("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(set_position);
 	}
 }
 
-change_colors();
+async function set_position(geolocation) {
+	lat = geolocation.coords.latitude;
+	lon = geolocation.coords.longitude;
+  
+	let api = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=2&appid=${open_weather_key}`;
+	await fetch(api)
+	.then(function (response) {
+		let data = response.json();
+		return data;
+	})
+	.then(function (data) {
+		city = data[0].name
+		country = data[0].country
+	})
+	.then(function () {
+		window.location = `/weather/${city}/${country}/${lat}/${lon}`;
+	});
+}
