67d76aa81933408035aa17fbcc9a5a8e50da04a9
m27bay <mickael.ledenmat@gmail.com>
2022-05-16 16:42:21 +0200

    Changement librairie heure + correction cycle jour/nuit avec décalage horaire

--- files/public/js/utils.js
+++ files/public/js/utils.js
@@ -1,92 +1,9 @@
 /**
- * Return corresponding french day.
- *
- * @param {String} day english day.
- * @returns french day.
+ * 
+ * @param {*} lang 
+ * @param {*} country 
+ * @returns 
  */
-function transform_day(day) {
-  const map_day_en_to_fr = {
-    Mon: "Lundi",
-    Tue: "Mardi",
-    Wed: "Mercredi",
-    Thu: "Jeudi",
-    Fri: "Vendredi",
-    Sat: "Samedi",
-    Sun: "Dimanche",
-  };
-
-  return map_day_en_to_fr[day];
-}
-
-/**
- * Return corresponding french day but shortest version.
- *
- * @param {String} day english day.
- * @returns french day.
- */
-function transform_day_low(day) {
-  const map_day_en_to_fr = {
-    Mon: "Lun",
-    Tue: "Mar",
-    Wed: "Mer",
-    Thu: "Jeu",
-    Fri: "Ven",
-    Sat: "Sam",
-    Sun: "Dim",
-  };
-
-  return map_day_en_to_fr[day];
-}
-
-/**
- * Return corresponding french month.
- *
- * @param {*} month english month.
- * @returns french month.
- */
-function transform_month(month) {
-  const map_month_en_to_fr = {
-    Jan: "Janvier",
-    Feb: "Fevrier",
-    Mar: "Mars",
-    Apr: "Avril",
-    May: "Mai",
-    Jun: "Juin",
-    Jul: "Juillet",
-    Aug: "Aout",
-    Sep: "Septembre",
-    Oct: "Octobre",
-    Nov: "Novembre",
-    Dec: "Decembre",
-  };
-
-  return map_month_en_to_fr[month];
-}
-
-/**
- * Transform date in french version.
- *
- * @param {Date} date english version (ex Mer Mar 16).
- * @param {Number} mode shortest day or not.
- * @returns date in french version (ex Mer Mar 16 -> Mercredi 16 Mars or Mer 16 Mars).
- */
-function transform_date(date, mode = 0) {
-  var date_str = date.toDateString();
-  var table_date_str = date_str.split(" ");
-
-  if (mode == 0) {
-    return (
-      transform_day(table_date_str[0]) +
-      " " +
-      table_date_str[2] +
-      " " +
-      transform_month(table_date_str[1])
-    );
-  } else if (mode == 1) {
-    return transform_day_low(table_date_str[0]) + " " + table_date_str[2];
-  }
-}
-
 function transform_country(lang, country) {
   try {
     let region_names = new Intl.DisplayNames([lang], { type: "region" });
